4367ff773e062d041db4c37e8782419bfbfe4be3
mataviejitasgg <moy778rodrigues@gmail.com>
2025-04-07 17:56:08 -0700

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -1,57 +1,62 @@
-const createAutocomplete =({root, rederOption,onOptionSelect, inputValue, fetchData}) => {
-    root.innerHTML=`
-    <label><b>Busqueda</b> </label>
-    <inputo class="input" />
+const createAutoComplete = ({root, renderOption, onOptionSelect, inputValue, fetchData }) => {
+
+    root.innerHTML = `
+    <label><b>Busqueda </b></label>
+    <input class="input" />
     <div class="dropdown">
         <div class="dropdown-menu">
-            <div class="dropdown-content results">
-
-            </div>
+        <div class="dropdown-content results"></div>
         </div>
     </div>
-    `
-}
-const input = root.querySelector("input")
-const dropdown = root.querySelector(".dropdown")
-const resultWrapper = root.querySelector(".result")
+    `;
 
-const debounce = (func, delay = 1000) =>{
-    let timeoutld
-    return(...args)=> {
-        clearTimeout(timeoutld)
-        timeoutld=timeoutld(()=>{
-    func.apply(null,args)
-    }, delay)
- }
-}
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
 
-const onInput = async event =>{
-    const items = await fethData(event.target.value)
-    console.log ("movies", items)
 
-    if(items.lenght){
-        dropdown.classlist.remove("is-active")
-        return
+    const debounce = (func, delay = 1000) => {
+        let timeoutId
+        return(...args) => {
+            clearTimeout(timeoutId)
+            timeoutId = setTimeout(() => {
+                func.apply(null, args)
+            }, delay)
+        }
     }
-    resultsWrappers.innerHTML = " "
-    dropdown.classlist.add("is-active")
-    for (let item of items){
-        const option = document.createElement("a")
+    const onInput = async event => {
+        const items = await fetchData(event.target.value)
+        console.log("MOVIES: ", items)
 
-        option.classlist.add("dropdown-item")
-        option.innerHTML = renderOption(item)
-        option.addEventListener("click", () => {
-            dropdown.classlist.remove("is-active")
-            input.value = inputValue(item)
-            onOptionSelect(item)
-            console.log("OnMovieSelect")
-        })
-        resultWrapper.appendChild(option)
+        if(!items.length){
+            dropdown.classList.remove('is-active')
+            return
+        }
+
+        resultsWrapper.innerHTML = ''
+        dropdown.classList.add('is-active')
+        for(let item of items) {
+            // const div = document.createElement('div')
+            const option = document.createElement('a')
+
+            option.classList.add('dropdown-item')
+            option.innerHTML = renderOption(item)            
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active')
+                input.value = inputValue(item)
+                onOptionSelect(item)
+                console.log("onMovieSelect")
+            })
+            // document.querySelector('#target').appendChild(div)
+            resultsWrapper.appendChild(option)
+        }
     }
-    input.addEventListener("input", debounce(onInput, 1000))
-    document.addEventListener("click", event => {
-        if(!root.contains(event.target)){
-            dropdown.classlist.remove("is-active")
+    input.addEventListener('input', debounce(onInput, 1000))
+
+    document.addEventListener('click', event => {
+        if(!root.contains(event.target)) {
+            dropdown.classList.remove('is-active')
         }
     })
+
 }
